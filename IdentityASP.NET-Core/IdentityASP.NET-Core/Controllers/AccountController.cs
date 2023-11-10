7a0a0fad35a0400e74e36cd768748c1f71c2ece0
saq52b <saqibjaved1112@gmail.com>
2023-11-10 09:05:49 -0800

    wip

---
+++ IdentityASP.NET-Core/IdentityASP.NET-Core/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using IdentityASP.NET_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityASP.NET_Core.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register() { 
+             RegisterViewModel registerViewModel=new RegisterViewModel();
+            return View(registerViewModel);
+        }
+    }
+}
